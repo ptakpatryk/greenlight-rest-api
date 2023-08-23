338d616a1c6a98da741669cab8250056419ce636
Patryk Ptak <ptaszqq@gmail.com>
2023-08-23 17:42:28 +0100

    Runtime unmarshal + validation

---
+++ internal/validator/validator.go
@@ -0,0 +1,56 @@
+package validator
+
+import "regexp"
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+type Validator struct {
+	errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{errors: make(map[string]string)}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.errors[key]; !exists {
+		v.errors[key] = message
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// Matches returns true if a string value matches a specific regexp pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+// Generic function which returns true if all values in a slice are unique.
+func Unique[T comparable](values []T) bool {
+	uniqueValues := make(map[T]bool)
+
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(values) == len(uniqueValues)
+}
